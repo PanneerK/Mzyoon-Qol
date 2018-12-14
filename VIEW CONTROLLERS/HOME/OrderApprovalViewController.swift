8e3a31bf8b8981089b32509816c461609c3a4370
Rohith Singh <rohith.p.singh@gmail.com>
2018-12-14 19:56:12 +0530

    Final commit - 14/12/2018  -  Rohith

--- VIEW CONTROLLERS/HOME/OrderApprovalViewController.swift
+++ VIEW CONTROLLERS/HOME/OrderApprovalViewController.swift
@@ -54,7 +54,63 @@ class OrderApprovalViewController: CommonViewController
         navigationTitle.font = UIFont(name: "Avenir-Regular", size: 20)
         orderApprovalNavigationBar.addSubview(navigationTitle)
         
-        PricingButton.frame = CGRect(x: 0, y: orderApprovalNavigationBar.frame.maxY, width: ((view.frame.width / 2) - 1), height: 50)
+        let DressDetView = UIView()
+        DressDetView.frame = CGRect(x: x + 15 , y: orderApprovalNavigationBar.frame.maxY + y, width: view.frame.width - (5 * x), height: (12 * y))
+        DressDetView.layer.cornerRadius = 5
+        DressDetView.layer.borderWidth = 1
+        DressDetView.layer.backgroundColor = UIColor.orange.cgColor
+        DressDetView.layer.borderColor = UIColor.black.cgColor
+        view.addSubview(DressDetView)
+        
+        let DressImageView = UIImageView()
+        DressImageView.frame = CGRect(x: x, y: y, width: (10 * x), height:(10 * y))
+        DressImageView.backgroundColor = UIColor.gray
+        DressDetView.addSubview(DressImageView)
+        
+        let DressTypeLabel = UILabel()
+        DressTypeLabel.frame = CGRect(x: DressImageView.frame.maxX + x, y: 0, width: (30 * x), height: (4 * y))
+        DressTypeLabel.text = "Men's Mandarin Suits "
+        DressTypeLabel.textColor = UIColor.white
+        DressTypeLabel.textAlignment = .left
+        DressTypeLabel.font = DressTypeLabel.font.withSize(16)
+        DressDetView.addSubview(DressTypeLabel)
+        
+        
+        let ColorLabel = UILabel()
+        ColorLabel.frame = CGRect(x: DressImageView.frame.maxX + x, y: DressTypeLabel.frame.minY + (2.5 * y), width: (8 * x), height: (4 * y))
+        ColorLabel.text = "Color  : "
+        ColorLabel.textColor = UIColor.white
+        ColorLabel.textAlignment = .left
+        ColorLabel.font = ColorLabel.font.withSize(14)
+        DressDetView.addSubview(ColorLabel)
+        
+        let ColorTypeLabel = UILabel()
+        ColorTypeLabel.frame = CGRect(x: ColorLabel.frame.maxX - (2 * x), y: DressTypeLabel.frame.minY + (2.5 * y), width: (8 * x), height: (4 * y))
+        ColorTypeLabel.text = "Grey"
+        ColorTypeLabel.textColor = UIColor.white
+        ColorTypeLabel.textAlignment = .left
+        ColorTypeLabel.font = ColorLabel.font.withSize(14)
+        DressDetView.addSubview(ColorTypeLabel)
+        
+        
+        let QtyLabel = UILabel()
+        QtyLabel.frame = CGRect(x: DressImageView.frame.maxX + x, y: ColorLabel.frame.minY + (2.5 * y), width: (8 * x), height: (4 * y))
+        QtyLabel.text = "Qty     : "
+        QtyLabel.textColor = UIColor.white
+        QtyLabel.textAlignment = .left
+        QtyLabel.font = ColorLabel.font.withSize(14)
+        DressDetView.addSubview(QtyLabel)
+        
+        
+        let QtyNumTF = UITextField()
+        QtyNumTF.frame = CGRect(x: QtyLabel.frame.maxX - (2 * x), y: ColorTypeLabel.frame.minY + (2.5 * y), width: (5 * x), height: (4 * y))
+        QtyNumTF.text = "1"
+        QtyNumTF.textColor = UIColor.white
+        QtyNumTF.textAlignment = .left
+        QtyNumTF.font = ColorLabel.font.withSize(14)
+        DressDetView.addSubview(QtyNumTF)
+        
+        PricingButton.frame = CGRect(x: 0, y: DressDetView.frame.maxY + y, width: ((view.frame.width / 2) - 1), height: 50)
         PricingButton.backgroundColor = UIColor(red: 0.0392, green: 0.2078, blue: 0.5922, alpha: 1.0)
         PricingButton.setTitle("PRICE DETAILS", for: .normal)
         PricingButton.setTitleColor(UIColor.white, for: .normal)
@@ -62,7 +118,7 @@ class OrderApprovalViewController: CommonViewController
         PricingButton.addTarget(self, action: #selector(self.selectionViewButtonAction(sender:)), for: .touchUpInside)
         view.addSubview(PricingButton)
         
-        DeliveryDetailsButton.frame = CGRect(x: PricingButton.frame.maxX + 1, y: orderApprovalNavigationBar.frame.maxY, width: view.frame.width / 2, height: 50)
+        DeliveryDetailsButton.frame = CGRect(x: PricingButton.frame.maxX + 1, y: DressDetView.frame.maxY + y, width: view.frame.width / 2, height: 50)
         DeliveryDetailsButton.backgroundColor = UIColor.lightGray
         DeliveryDetailsButton.setTitle("DELIVERY DETAILS", for: .normal)
         DeliveryDetailsButton.setTitleColor(UIColor.black, for: .normal)
@@ -72,8 +128,8 @@ class OrderApprovalViewController: CommonViewController
         
         DeliveryDetailsButton.backgroundColor = UIColor.lightGray
         DeliveryDetailsButton.setTitleColor(UIColor.black, for: .normal)
-          PricingViewContents(isHidden: false)
-          DeliveryDetailsViewContents(isHidden: true)
+        PricingViewContents(isHidden: false)
+        DeliveryDetailsViewContents(isHidden: true)
        
     }
     
@@ -116,67 +172,51 @@ class OrderApprovalViewController: CommonViewController
     func PricingViewContents(isHidden : Bool)
     {
         
-        let DressDetView = UIView()
-        DressDetView.frame = CGRect(x: x + 15 , y: DeliveryDetailsButton.frame.maxY + y, width: view.frame.width - (5 * x), height: (12 * y))
-        DressDetView.layer.cornerRadius = 5
-        DressDetView.layer.borderWidth = 1
-        DressDetView.layer.backgroundColor = UIColor.orange.cgColor
-        DressDetView.layer.borderColor = UIColor.black.cgColor
-        view.addSubview(DressDetView)
-        
-        let DressImageView = UIImageView()
-        DressImageView.frame = CGRect(x: x, y: y, width: (10 * x), height:(10 * y))
-        DressImageView.backgroundColor = UIColor.gray
-        DressDetView.addSubview(DressImageView)
-        
-        let DressTypeLabel = UILabel()
-        DressTypeLabel.frame = CGRect(x: DressImageView.frame.maxX + x, y: 0, width: (30 * x), height: (4 * y))
-        DressTypeLabel.text = "Men's Mandarin Suits "
-        DressTypeLabel.textColor = UIColor.white
-        DressTypeLabel.textAlignment = .left
-        DressTypeLabel.font = DressTypeLabel.font.withSize(16)
-        DressDetView.addSubview(DressTypeLabel)
-        
        
-        let ColorLabel = UILabel()
-        ColorLabel.frame = CGRect(x: DressImageView.frame.maxX + x, y: DressTypeLabel.frame.minY + (2.5 * y), width: (8 * x), height: (4 * y))
-        ColorLabel.text = "Color  : "
-        ColorLabel.textColor = UIColor.white
-        ColorLabel.textAlignment = .left
-        ColorLabel.font = ColorLabel.font.withSize(14)
-        DressDetView.addSubview(ColorLabel)
-        
-        let ColorTypeLabel = UILabel()
-        ColorTypeLabel.frame = CGRect(x: ColorLabel.frame.maxX - (2 * x), y: DressTypeLabel.frame.minY + (2.5 * y), width: (8 * x), height: (4 * y))
-        ColorTypeLabel.text = "Grey"
-        ColorTypeLabel.textColor = UIColor.white
-        ColorTypeLabel.textAlignment = .left
-        ColorTypeLabel.font = ColorLabel.font.withSize(14)
-        DressDetView.addSubview(ColorTypeLabel)
-        
-        
-        let QtyLabel = UILabel()
-        QtyLabel.frame = CGRect(x: DressImageView.frame.maxX + x, y: ColorLabel.frame.minY + (2.5 * y), width: (8 * x), height: (4 * y))
-        QtyLabel.text = "Qty     : "
-        QtyLabel.textColor = UIColor.white
-        QtyLabel.textAlignment = .left
-        QtyLabel.font = ColorLabel.font.withSize(14)
-        DressDetView.addSubview(QtyLabel)
-        
-        
-        let QtyNumTF = UITextField()
-        QtyNumTF.frame = CGRect(x: QtyLabel.frame.maxX - (2 * x), y: ColorTypeLabel.frame.minY + (2.5 * y), width: (5 * x), height: (4 * y))
-        QtyNumTF.text = "1"
-        QtyNumTF.textColor = UIColor.white
-        QtyNumTF.textAlignment = .left
-        QtyNumTF.font = ColorLabel.font.withSize(14)
-        DressDetView.addSubview(QtyNumTF)
     }
     
     func DeliveryDetailsViewContents(isHidden : Bool)
     {
         
+        let deliveryDetailsView = UIView()
+        deliveryDetailsView.frame = CGRect(x: (4 * x), y: DeliveryDetailsButton.frame.maxY + (2 * y), width: view.frame.width - (6 * x), height: (28 * x))
+        deliveryDetailsView.backgroundColor = UIColor.white
+        view.addSubview(deliveryDetailsView)
         
+     //   let dressTypeArray = ["Appointments  -  Nil", "Delivery Type  -  Urgent", "Stiching time required for stiches  -  4 days", "Approximate Delivery Date  -  24-12-2018 to 28-12-2018"]
+        
+        var y1:CGFloat = y
+        
+     
+     //   for i in 0..<dressTypeArray.count
+      //  {
+            let AppointmentViewLabels = UILabel()
+            AppointmentViewLabels.frame = CGRect(x: x, y: y1 + 2, width: deliveryDetailsView.frame.width - (2 * x), height: (6 * y))
+            AppointmentViewLabels.layer.cornerRadius = 10
+            AppointmentViewLabels.layer.masksToBounds = true
+            AppointmentViewLabels.backgroundColor = UIColor(red: 0.0392, green: 0.2078, blue: 0.5922, alpha: 1.0)
+          //  DeliveryDetLabels.text = dressTypeArray[i]
+           // AppointmentViewLabels.textColor = UIColor.white
+           // AppointmentViewLabels.textAlignment = .left
+           // AppointmentViewLabels.font = AppointmentViewLabels.font.withSize(14)
+            deliveryDetailsView.addSubview(AppointmentViewLabels)
+            
+            y1 = AppointmentViewLabels.frame.maxY + (y / 2)
+        
+        
+           let AppointmentsLabels = UILabel()
+           AppointmentsLabels.frame = CGRect(x: 0, y: 0 , width: 20 * x, height: (3 * y))
+          // AppointmentsLabels.backgroundColor = UIColor(red: 0.0392, green: 0.2078, blue: 0.5922, alpha: 1.0)
+           AppointmentsLabels.text = "Appointments          - "
+          AppointmentsLabels.textColor = UIColor.white
+          AppointmentsLabels.textAlignment = .left
+          AppointmentsLabels.font = AppointmentsLabels.font.withSize(14)
+          AppointmentViewLabels.addSubview(AppointmentsLabels)
+        
+        
+      //  }
+        
+     
         
     }
 
