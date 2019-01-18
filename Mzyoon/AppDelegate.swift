274cd6c059d78aa5cc7579cb017d8c8ad36ba588
Rohith Singh <rohith.p.singh@gmail.com>
2019-01-18 21:06:56 +0530

    final Commit  - rohtih

--- Mzyoon/AppDelegate.swift
+++ Mzyoon/AppDelegate.swift
@@ -39,7 +39,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 navigationScreen.isNavigationBarHidden = true
                 window?.rootViewController = navigationScreen
                 window?.makeKeyAndVisible()
-
+ 
             }
             else
             {
