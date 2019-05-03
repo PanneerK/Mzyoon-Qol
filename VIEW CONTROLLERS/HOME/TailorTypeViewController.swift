8c82ae9f90725e17586f029f9d0aaccec6f6a2fb
Rohith Singh <rohith.p.singh@gmail.com>
2019-05-03 20:28:00 +0530

    Final commit Changes Done

--- VIEW CONTROLLERS/HOME/TailorTypeViewController.swift
+++ VIEW CONTROLLERS/HOME/TailorTypeViewController.swift
@@ -112,33 +112,69 @@ class TailorTypeViewController: CommonViewController,ServerAPIDelegate
         quoteButton.frame = CGRect(x: x, y: pageBar.frame.maxY + y, width: view.frame.width - (2 * x), height: (20 * y))
         quoteButton.layer.cornerRadius = x
         quoteButton.backgroundColor = UIColor(red: 0.0392, green: 0.2078, blue: 0.5922, alpha: 1.0)
-        quoteButton.setTitle("GET A QUOTE FROM TAILORS", for: .normal)
+        quoteButton.setTitle(TailorTypeEnglishNameArray[0] as? String, for: .normal)
         quoteButton.setTitleColor(UIColor.white, for: .normal)
         quoteButton.contentVerticalAlignment = .bottom
+        
+        let quoteButtonImage = UIImageView()
+        quoteButtonImage.frame = CGRect(x: 0, y: 0, width: quoteButton.frame.width, height: quoteButton.frame.height - (2 * y))
+        if let imageName =  TailorTypeImageArray[0] as? String
+        {
+            let urlString = serviceCall.baseURL
+            let api = "\(urlString)/images/TailorList/\(imageName)"
+            let apiurl = URL(string: api)
+            if apiurl != nil
+            {
+                quoteButtonImage.dowloadFromServer(url: apiurl!)
+            }
+        }
+        quoteButtonImage.contentMode = .scaleToFill
+        quoteButton.addSubview(quoteButtonImage)
+        
         quoteButton.tag = 1
         quoteButton.addTarget(self, action: #selector(self.tailorTypeButtonACtion(sender:)), for: .touchUpInside)
         view.addSubview(quoteButton)
         
+        /*
         let quoteButtonImage = UIImageView()
         quoteButtonImage.frame = CGRect(x: 0, y: 0, width: quoteButton.frame.width, height: quoteButton.frame.height - (2 * y))
         quoteButtonImage.image = UIImage(named: "")
         quoteButton.addSubview(quoteButtonImage)
+        */
         
         let directButton = UIButton()
         directButton.frame = CGRect(x: x, y: quoteButton.frame.maxY + y, width: view.frame.width - (2 * x), height: (20 * y))
         directButton.layer.cornerRadius = x
         directButton.backgroundColor = UIColor(red: 0.0392, green: 0.2078, blue: 0.5922, alpha: 1.0)
-        directButton.setTitle("SELECT A TAILOR FOR DIRECT ORDER", for: .normal)
+        directButton.setTitle(TailorTypeEnglishNameArray[1] as? String, for: .normal)
         directButton.setTitleColor(UIColor.white, for: .normal)
         directButton.contentVerticalAlignment = .bottom
+        
+        let directButtonImage = UIImageView()
+        directButtonImage.frame = CGRect(x: 0, y: 0, width: directButton.frame.width, height: directButton.frame.height - (2 * y))
+        if let imageName =  TailorTypeImageArray[1] as? String
+        {
+            let urlString = serviceCall.baseURL
+            let api = "\(urlString)/images/TailorList/\(imageName)"
+            let apiurl = URL(string: api)
+            if apiurl != nil
+            {
+                directButtonImage.dowloadFromServer(url: apiurl!)
+            }
+        }
+        directButtonImage.contentMode = .scaleToFill
+        directButton.addSubview(directButtonImage)
+        
         directButton.tag = 2
         directButton.addTarget(self, action: #selector(self.tailorTypeButtonACtion(sender:)), for: .touchUpInside)
         view.addSubview(directButton)
         
+        /*
         let directButtonImage = UIImageView()
         directButtonImage.frame = CGRect(x: 0, y: 0, width: directButton.frame.width, height: directButton.frame.height - (2 * y))
         directButtonImage.image = UIImage(named: "")
         directButton.addSubview(directButtonImage)
+        */
     }
     
     @objc func otpBackButtonAction(sender : UIButton)
