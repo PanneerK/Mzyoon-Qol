f669201acab3c2fb3f6542ea456bda681ea3f799
Rohith Singh <rohith.p.singh@gmail.com>
2019-05-13 18:33:57 +0530

    final commit 13/5/2019

--- VIEW CONTROLLERS/APPOINTMENTS/PaymentViewController.swift
+++ VIEW CONTROLLERS/APPOINTMENTS/PaymentViewController.swift
@@ -116,7 +116,9 @@ class PaymentViewController: CommonViewController,ServerAPIDelegate,UITextFieldD
     override func viewDidLoad()
     {
         super.viewDidLoad()
-      
+        
+        selectedButton(tag: 1)
+        
         print("TailorId",TailorId)
         print("Global TailorID:",Variables.sharedManager.TailorID)
       
