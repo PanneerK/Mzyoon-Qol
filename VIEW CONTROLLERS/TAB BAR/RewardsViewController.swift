b587664ab1eda672767ada2119db6a630f4d02e0
Rohith Singh <rohith.p.singh@gmail.com>
2019-05-20 18:58:42 +0530

    Rewards s Commit 16/5/2019

---
+++ VIEW CONTROLLERS/TAB BAR/RewardsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RewardsViewController.swift
+//  Mzyoon
+//
+//  Created by QOLSoft on 17/05/19.
+//  Copyright © 2019 QOL. All rights reserved.
+//
+
+import UIKit
+
+class RewardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
